1cf043f339398de1d0add52b2c7ba22f0ad79c13
Christian <christiandp7@gmail.com>
2020-05-22 12:36:26 -0400

    Adding Canvas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, NavLink, Route } from 'react-router-dom'
 
 import Weather from './components/Weather'
+import Canvas from './components/Canvas'
 
 import './App.css'
 
@@ -11,7 +12,8 @@ function App() {
       <header className="App-header">
         <div className="container contheader">
           <ul>
-          <li><NavLink activeClassName="active" to="/">Weather</NavLink></li>
+          <li><NavLink activeClassName="active" exact to="/">Weather</NavLink></li>
+          <li><NavLink activeClassName="active" to="/canvas">Canvas</NavLink></li>
           </ul>
         </div>
         
@@ -21,6 +23,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Weather />
+          </Route>
+          <Route exact path="/canvas"> 
+            <Canvas />
           </Route>
           <Route path="*">
             <h1>404 - Not Found</h1>
