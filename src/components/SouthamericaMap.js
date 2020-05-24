2099b47d26f94b80bb2731c76595a4e1d017cf96
Christian <christiandp7@gmail.com>
2020-05-24 12:18:59 -0400

    Adding Southamerica Map

--- src/components/SouthamericaMap.js
+++ src/components/SouthamericaMap.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { VectorMap } from '@south-paw/react-vector-maps'
 
-import southamerica from '../assets/maps/southamerica-new.json'
+import southamerica from '../assets/maps/sudamerica.json'
 
 function SouthamericaMap() {
 
   const style = { margin: '1rem auto', width: '400px' };
 
   return (
-    <div style={style}>
+    <div className="sudamerica-map" style={style}>
       <VectorMap {...southamerica} />
     </div>
   )
