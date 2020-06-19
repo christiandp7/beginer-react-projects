02dd0b63ec335cd8c652f05982276933e955ce2e
Christian <christiandp7@gmail.com>
2020-06-18 23:24:11 -0400

    init UseEffect

--- src/components/UseEffect.js
+++ src/components/UseEffect.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function UseEffect() {
+
+  const [resourseType, setResourceType] = useState('posts')
+
   return (
-    <div>
-      
-    </div>
+    <>
+      <button onClick={() => setResourceType('posts')} className="btn btn-primary">Posts</button>
+      <button onClick={() => setResourceType('users')} className="btn btn-primary">Users</button>
+      <button onClick={() => setResourceType('comments')} className="btn btn-primary">Comments</button>
+      <h1>{resourseType}</h1>
+    </>
   )
 }
 
