3e6d3d1cbdd7f00b19179570d7e6f9219120ba78
Christian <christiandp7@gmail.com>
2020-11-02 14:27:43 -0400

    Playing around with glide events

--- src/components/Glide.js
+++ src/components/Glide.js
@@ -6,7 +6,18 @@ import Glidejs from '@glidejs/glide'
 
 function Glide() {
 
+
+  const modifyTranslate = function (Glide, Components, Events) {
+    return {
+      modify (translate) {
+        //return translate - 100
+        return translate
+      }
+    }
+  }
+
   useEffect(() => {
+
     const glide = new Glidejs(".glide", {
       peek: { before: 0, after: 70 },
       perView: 3,
@@ -16,7 +27,32 @@ function Glide() {
       //focusAt: 3,
       direction: 'rtl'
     });
-    glide.mount();
+
+    /*glide.on(['mount.after'], function(param) {
+      const preActiveElm = document.querySelector(".glide__slide--active");
+      preActiveElm.classList.add("activated")
+    })*/
+
+    //glide.on(['run.before'], function(param) {
+    //glide.on('run', function(param) {
+    glide.on('swipe.end', function(param) {
+      console.log(glide.index);
+      console.log(param)
+      /*const preActiveElm = document.querySelector(".glide__slide--active").nextElementSibling;
+      preActiveElm.classList.add("activated")*/
+    })
+
+    glide.on(['run.after'], function(param) {
+      /*const activatedElms = document.querySelector(".glide__slide--active");
+      activatedElms.classList.remove("activated")*/
+      /*activatedElms.forEach(function(elm) {
+        elm.classList.remove("activated")
+      })*/
+      //activatedElms.classList.remove("activated")
+    })
+
+    glide.mutate([modifyTranslate]).mount();
+
     return () => {
       //console.log("glide unmounted")
       glide.destroy();
@@ -26,26 +62,26 @@ function Glide() {
   
 
   return (
-    <div class="glide multi">
-      <div class="glide__arrows" data-glide-el="controls">
-        {/*<button class="glide__arrow glide__arrow--left btn btn-primary" data-glide-dir=">">
+    <div className="glide multi">
+      <div className="glide__arrows" data-glide-el="controls">
+        {/*<button className="glide__arrow glide__arrow--left btn btn-primary" data-glide-dir=">">
           Prev
   </button>*/}
       </div>
-      <div class="glide__track" data-glide-el="track">
-        <ul class="glide__slides">
-          <li class="glide__slide">1</li>
-          <li class="glide__slide">2</li>
-          <li class="glide__slide">3</li>
-          <li class="glide__slide">4</li>
-          <li class="glide__slide">5</li>
+      <div className="glide__track" data-glide-el="track">
+        <ul className="glide__slides">
+          <li className="glide__slide">1</li>
+          <li className="glide__slide">2</li>
+          <li className="glide__slide">3</li>
+          <li className="glide__slide">4</li>
+          <li className="glide__slide">5</li>
         </ul>
       </div>
-      <div class="glide__arrows" data-glide-el="controls">
-      <button class="glide__arrow glide__arrow--right btn btn-primary" data-glide-dir="<">
+      <div className="glide__arrows" data-glide-el="controls">
+      <button className="glide__arrow glide__arrow--right btn btn-primary" data-glide-dir="<">
           Next
       </button>
-        {/*<button class="glide__arrow glide__arrow--right btn btn-primary" data-glide-dir="<">
+        {/*<button className="glide__arrow glide__arrow--right btn btn-primary" data-glide-dir="<">
           Next
   </button>*/}
       </div>
